c893787d9b86a3a3c62e5a3f7fa0a9c81ce93cbd
MrFellox <jfernandohernandez28@gmail.com>
2022-02-14 11:50:57 -0600

    You can now delete tasks

--- todo/routes.py
+++ todo/routes.py
@@ -1,3 +1,4 @@
+from importlib.resources import read_text
 from flask import render_template, request, redirect, url_for, flash
 from todo.forms import AddTaskForm, DeleteTaskForm
 from todo import app
@@ -28,7 +29,25 @@ def index():
 
         return redirect(url_for('index'))
 
-    elif delete_task_form.validate_on_submit():
-        pass
+    if delete_task_form.validate_on_submit():
+
+        task_id = delete_task_form.task_id.data
+
+        try:
+            task_obj = Task.query.filter_by(id=task_id).first()
+
+            if task_obj is None:
+                flash('Task not found.', 'danger')
+
+            else:
+                db.session.delete(task_obj)
+                db.session.commit()
+
+                flash('Task deleted successfully!', 'success')
+                redirect(url_for('index'))
+
+        except Exception as e:
+            flash('There was an error while deleting the task.', 'danger')
+            print(traceback.print_exc())
 
     return render_template('index.html', add_task_form=add_task_form, delete_task_form=delete_task_form, tasks=Task.query.order_by(Task.id.desc()).all())
